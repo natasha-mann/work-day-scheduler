66b84e17a6f74af84a7570b25bb4ff8086eda07b
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-28 12:59:32 +0100

    added time from moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
+//get current time from moment
 const getCurrentDateTime = () => {
   const currentDateTime = moment().format("dddd Do MMMM, HH:mm:ss");
-  console.log(currentDateTime);
+  $("#currentDay").text(currentDateTime);
   return currentDateTime;
 };
 
-const onLoad = () => {
-  //get current time from moment
-  getCurrentDateTime();
-  // append current time to header
+// append current time to header + start clock timer
+const startTimer = () => {
+  const timerTick = () => {
+    const currentDateTimeText = getCurrentDateTime();
+    $("#currentDay").text(currentDateTimeText);
+  };
+  setInterval(timerTick, 1000);
+};
 
-  // set colour of time blocks
+const onLoad = () => {
+  getCurrentDateTime();
+
+  startTimer();
 };
 
 $(document).ready(onLoad);
