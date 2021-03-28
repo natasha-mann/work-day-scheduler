4c7814620045a04433c0db2ef39edf0a51525b06
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-28 17:47:13 +0100

    added function to store tasks in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ const daySchedulerContainer = $(".container");
 const textAreas = daySchedulerContainer.children().children("textarea");
 
 //get current time from moment
-const getCurrentDateTime = () => {
-  const currentDateTime = moment().format("dddd Do MMMM");
-  $("#currentDay").text(currentDateTime);
-  return currentDateTime;
+const getCurrentDate = () => {
+  const currentDate = moment().format("dddd Do MMMM");
+  $("#currentDay").text(currentDate);
+  return currentDate;
 };
 
 // set colour of time blocks to show past/present/future
@@ -29,10 +29,32 @@ const setColor = (index) => {
   }
 };
 
+// Save hourly tasks to local storage
+const storeHourlyTasks = (event) => {
+  const target = $(event.target);
+
+  if (target.is("button")) {
+    const hour = target.siblings("textarea").dataset.time;
+    console.log(hour);
+  }
+};
+
+// Get hourly tasks from local storage
+const getHourlyTasksFromLocalStorage = () => {
+  let hourlyTasks = localStorage.getItem("hourlyTasks");
+  if (hourlyTasks) {
+    return JSON.parse(hourlyTasks);
+  } else {
+    return {};
+  }
+};
+
 // when the page loads
 const onLoad = () => {
-  getCurrentDateTime();
+  getCurrentDate();
   colorCodeTimeBlocks();
+  getHourlyTasksFromLocalStorage();
 };
 
 $(document).ready(onLoad);
+$(daySchedulerContainer).on("click", storeHourlyTasks);
