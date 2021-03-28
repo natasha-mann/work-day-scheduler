12f31f2f4093736e8355c07488389b72f99e342d
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-28 16:40:22 +0100

    added function to change color of time blocks in reference to current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,37 @@
+const daySchedulerContainer = $(".container");
+const textAreas = daySchedulerContainer.children().children("textarea");
+
 //get current time from moment
 const getCurrentDateTime = () => {
-  const currentDateTime = moment().format("dddd Do MMMM, HH:mm:ss");
+  const currentDateTime = moment().format("dddd Do MMMM");
   $("#currentDay").text(currentDateTime);
   return currentDateTime;
 };
 
-// append current time to header + start clock timer
-const startTimer = () => {
-  const timerTick = () => {
-    const currentDateTimeText = getCurrentDateTime();
-    $("#currentDay").text(currentDateTimeText);
-  };
-  setInterval(timerTick, 1000);
-};
-
-// remove AM/PM to get hour from time block
-const getHourFromTimeBlock = (row) => {
-  const onlyNumbers = row.innerText.slice(0, -2);
-  return [onlyNumbers];
-};
-
-// get array of time blocks from html
-const getTimeBlockArray = () => {
-  const timeBlocks = $(".container .row .hour");
-  const timeBlockArray = $.map(timeBlocks, getHourFromTimeBlock);
-  return timeBlockArray;
-};
-
 // set colour of time blocks to show past/present/future
 const colorCodeTimeBlocks = () => {
-  const currentHour = moment().format("HH");
+  textAreas.each(setColor);
 };
 
+const setColor = (index) => {
+  const currentHour = parseInt(moment().format("HH"));
+  const timeBlockHour = parseInt(textAreas[index].dataset.time);
+
+  if (timeBlockHour < currentHour) {
+    $(textAreas[index]).removeClass("future present");
+    $(textAreas[index]).addClass("past");
+  } else if (timeBlockHour === currentHour) {
+    $(textAreas[index]).removeClass("past future");
+    $(textAreas[index]).addClass("present");
+  } else {
+    $(textAreas[index]).removeClass("present past");
+    $(textAreas[index]).addClass("future");
+  }
+};
+
+// when the page loads
 const onLoad = () => {
   getCurrentDateTime();
-  startTimer();
   colorCodeTimeBlocks();
 };
 
